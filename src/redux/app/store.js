45c0e15c3fec689b43523840e507d7523d236230
otegaa <ekiugbotega@gmail.com>
2023-05-10 16:36:09 +0200

    Implement checkStatus reducer for category slice

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from '../features/books/BookSlice';
+import categoryReducer from '../features/category/CategorySlice';
 
 const store = configureStore({
   reducer: {
     book: bookReducer,
+    category: categoryReducer,
   },
 });
 
