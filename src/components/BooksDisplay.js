2b5654f345fd7c1ead2d9aa235f27e5e61a26b86
otegaa <ekiugbotega@gmail.com>
2023-05-11 15:32:16 +0200

    Implement fetching data from API using createAsyncThunk

--- src/components/BooksDisplay.js
+++ src/components/BooksDisplay.js
@@ -1,28 +1,40 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBooks } from '../redux/books/BookSlice';
+import { useEffect } from 'react';
+import { fetchBooks } from '../redux/books/BookSlice';
 
 const BooksDisplay = () => {
-  const { books } = useSelector((store) => store.book);
+  const { books, loading, error } = useSelector((store) => store.book);
+  console.log(books);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
+
   return (
     <>
-      {books.map((book) => {
-        const {
-          author, title, id, category,
-        } = book;
-        return (
-          <ul key={id}>
-            <li style={{ marginBottom: '2rem' }}>
-              <h2 style={{ fontSize: '2.5rem' }}>{title}</h2>
-              <h4 style={{ fontSize: '2rem' }}>{author}</h4>
-              <h4 style={{ fontSize: '2rem' }}>{category}</h4>
-              <button type="button" onClick={() => dispatch(removeBooks(id))}>
-                Delete
-              </button>
-            </li>
+      <div>
+        <h2>Books</h2>
+        {loading && <div>Loading...</div>}
+        {!loading && error ? <div>Error: error</div> : null}
+        {!loading && books.length > 0 ? (
+          <ul>
+            {books.map((book) => {
+              const {
+                id, title, author, category,
+              } = book;
+              return (
+                <li style={{ marginBottom: '2rem' }} key={id}>
+                  <h2 style={{ fontSize: '2.5rem' }}>{title}</h2>
+                  <h4 style={{ fontSize: '2rem' }}>{author}</h4>
+                  <h4 style={{ fontSize: '2rem' }}>{category}</h4>
+                  <button type="button">Delete</button>
+                </li>
+              );
+            })}
           </ul>
-        );
-      })}
+        ) : null}
+      </div>
     </>
   );
 };
--- src/redux/books/BookSlice.js
+++ src/redux/books/BookSlice.js
@@ -1,8 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
-import booksList from '../../components/data';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseUrl = ' https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+
+const appId = 'hdYVDIhRkCXTLriBlu9I';
+
+export const fetchBooks = createAsyncThunk(
+  'book/fetchBooks',
+  async (thunkAPI) => {
+    try {
+      const res = await axios.get(`${baseUrl}${appId}/books`);
+      const data = await res.data;
+      console.log(data);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
 
 const initialState = {
-  books: booksList,
+  loading: false,
+  books: [],
+  error: '',
 };
 
 const bookSlice = createSlice({
@@ -15,6 +35,22 @@ const bookSlice = createSlice({
     removeBooks: (state, { payload }) => {
       state.books = state.books.filter((book) => book.id !== payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchBooks.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.books = payload;
+        state.error = '';
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.loading = false;
+        state.books = [];
+        state.error = action.error.message;
+      });
   },
 });
 
